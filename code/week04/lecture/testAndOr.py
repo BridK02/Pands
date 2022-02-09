28444a3fcb56ac6d2fae4b9b15e92000e29f941a
Brid Callaghan <bridcallaghan56@gmail.com>
2022-02-09 22:13:06 +0000

    week 04

---
+++ code/week04/lecture/testAndOr.py
@@ -0,0 +1,7 @@
+# messing with ands and ors
+# Author: Brid Kennedy
+
+
+number = 77
+if (number >=0 ) and (number <= 100):
+    print("valid")
---
+++ code/week04/lecture/testBoolean.py
@@ -0,0 +1,12 @@
+#messing with boolean
+#Author: Brid Kennedy
+
+
+isAlive = False
+
+print(isAlive) 
+a = 32
+isEven = a % 2 == 0
+
+print ("is ", a, "equal?", isEven)
+
